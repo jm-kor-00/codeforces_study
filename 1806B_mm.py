b31c9d0f89609ff73088f4c19e47dd80051a463e
Jaemin Eun <tomy000120@naver.com>
2023-03-21 17:42:14 +0900

    0321

--- 1806B_mm.py
+++ 1806B_mm.py
@@ -5,15 +5,24 @@ input = sys.stdin.readline
 for _ in range(int(input())):
     N = int(input())
     arr = list(map(int,input().split()))
-    array = []; inArr = [0,0]
-    for i in range(N-1):
-        heapq.heappush(array,arr[i]+arr[i+1])
-    
-    initial = heapq.heappop(array)
-    if initial > 0 :
-        print(initial - 1);continue
-    
-    for i in range(N - 2):
-        tmp  = heapq.heappop(array)
-        if inArr[1] + 1 < tmp: break
-    print(inArr[1] + 1)
+
+    zero_count = 0
+    one_count = 0
+
+    for el in arr:
+        if el == 0: zero_count += 1
+        elif el == 1: one_count += 1
+
+    if N % 2 == 0: zeropos = N // 2
+    else : zeropos = N // 2 + 1
+
+    if zero_count == N : print(1)
+
+    elif zero_count <= zeropos:
+        print(0)
+        
+    else :
+        if one_count + zero_count == N:
+            print(2)           
+        else :
+            print(1)
